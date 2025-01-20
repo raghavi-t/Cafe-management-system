177a74f5e0943663cb1c46f0c6bd7f9852d1c14f
raghavi-t <74418851+raghavi-t@users.noreply.github.com>
2025-01-20 16:11:17 +0530

    Add files via upload

---
+++ Backend-master (1)/Backend-master/src/main/java/com/inn/cafe/constents/CafeConstants.java
@@ -0,0 +1,13 @@
+package com.inn.cafe.constents;
+
+public class CafeConstants {
+    public static final String SOMETHING_WENT_WRONG = "Something Went Wrong.";
+
+    public static final String INVALID_INFO = "Invalid Data.";
+
+    public static final String UNAUTHORIZED_ACCESS = "unauthorized access.";
+    public static final String INVALID_DATA = "Invalid Data.";
+    public static final String STORE_LOCATION = "C:\\Users\\HP\\Downloads\\Cafe-Management-System-Angular-SpringBoot-main\\Cafe-Management-System-Angular-SpringBoot-main\\Backend\\CafeStoredFiles";
+    //
+
+}
